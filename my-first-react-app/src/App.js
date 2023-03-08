0dd9c9e7322ce5b1433aa9c2f69f7a69215c942d
Karolina Dbaj <k.dbaj@outlook.com>
2023-03-08 14:08:57 +0100

    Functions as Props

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -4,12 +4,17 @@ import Title from "./components/Title";
 import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "movie night", id: 1 },
     { title: "birthday", id: 3 },
     { title: "live stream", id: 2 },
   ]);
+
+  console.log(showModal);
+
+  const subtitle = "All the latest events";
 
   const deleteEvent = (id) => {
     setEvents((prevEvents) => {
@@ -20,8 +25,9 @@ function App() {
     console.log(id);
   };
   //abc
-
-  const subtitle = "All the latest events";
+  const handleClose = () => {
+    setShowModal(false);
+  };
 
   useEffect(() => {
     console.log(showEvents);
@@ -31,29 +37,35 @@ function App() {
     <div className="App" id="test">
       <Title title="Events in Your Area" subtitle={subtitle} />
 
-      <Modal>
+      {/* <Modal>
         <h2>10% Off coupon Code!</h2>
         <p>Use our code at the checkout</p>
-      </Modal>
-      <Modal>
-        <h2>Terms and Conditions</h2>
-        <p>sakndjsnajdkjnasknjkanjnsjandjsan</p>
-        <a href="#">find out more!</a>
-      </Modal>
+      </Modal> */}
+
+      {showModal && (
+        <Modal handleClose={handleClose}>
+          <h2>Terms and Conditions</h2>
+          <p>sakndjsnajdkjnasknjkanjnsjandjsan</p>
+          <a href="#">find out more!</a>
+        </Modal>
+      )}
       <Title title="other title" subtitle="events" />
       {showEvents && (
         <div>
-          <button onClick={() => setShowEvents(false)}>hide events</button>
+          <button className="hideEvents" onClick={() => setShowEvents(false)}>
+            Hide events
+          </button>
         </div>
       )}
       {!showEvents && (
         <div>
           <button
+            className="showEvents"
             onClick={() => {
               setShowEvents(true);
             }}
           >
-            show events
+            Show events
           </button>
         </div>
       )}
--- my-first-react-app/src/components/Modal.jsx
+++ my-first-react-app/src/components/Modal.jsx
@@ -1,16 +1,16 @@
 import './Modal.css'
 
-export default function Modal({children}) {
+export default function Modal({children, handleClose}) {
   return (
     <div className="modal-backdrop">
         <div className="modal">
           {/* instead of writing everything in component
-          we just write it in main file
-          
+          we just write it in main file          
           children are elements of main component*/}
-          {children}
             {/* <h2>10% Off coupon Code!</h2>
             <p>Use our code at the checkout</p> */}
+          {children}
+          <button onClick={handleClose}>Close</button>
         </div>
 
     </div>
