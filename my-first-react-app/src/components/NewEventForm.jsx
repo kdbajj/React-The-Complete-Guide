6a7fb7e702f5b2aa902307f5fef024d1ab5c4528
Karolina Dbaj <k.dbaj@outlook.com>
2023-03-16 21:06:03 +0100

    The onChange Event

--- my-first-react-app/src/components/NewEventForm.jsx
+++ my-first-react-app/src/components/NewEventForm.jsx
@@ -1,23 +1,28 @@
+import { useState } from 'react';
 import './NewEventForm.css'
 
 
 export default function NewEventForm() {
 
-    const handleChange= (e)=>{
-        console.log(e.target.value);
-    }
+    const [title, setTitle] = useState('') 
+    const [date, setDate] = useState('')
+
+    // const handleChange= (e)=>{
+    //     setTitle(e.target.value)
+    // }
 
   return (
     <form className='new-event-form'>
         <label>
             <span className='eventTitle'>Event Title: </span>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" onChange={(e) => setTitle(e.target.value)} />
         </label>
     <label>
         <span className='eventDate'> Event Date:</span>
-        <input type="date"/>
+        <input type="date" onChange={(e) => setDate(e.target.value)}/>
     </label>
     <button>Submit</button>
+    <p>title - {title}, date - {date}</p>
     </form>
   )
 }
