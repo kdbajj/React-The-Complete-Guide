460b96a690f058423187a667951ff2ea06a9bb9f
Karolina Dbaj <k.dbaj@outlook.com>
2023-04-03 12:58:39 +0200

    Why We Need a Cleanup Function

--- tripago/src/App.js
+++ tripago/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import TripList from "./components/TripList";
 
 function App() {
+  const [showTrips, setShowtrips] = useState(true);
+
   return (
     <div className="App">
-      <TripList></TripList>
+      <button onClick={() => setShowtrips(!showTrips)}>Hide trips</button>
+      {showTrips && <TripList />}
     </div>
   );
 }
