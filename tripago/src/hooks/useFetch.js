0db693daa4712cf6cff951a0bbda096d80234381
Karolina Dbaj <k.dbaj@outlook.com>
2023-04-25 13:24:14 +0200

    useEffect Gotcha - Infinite Loops

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -1,11 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, _options) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
 
+  const options = useRef(_options).current;
+
   useEffect(() => {
+    console.log(options);
     const controller = new AbortController();
 
     const fetchData = async () => {
@@ -35,7 +38,7 @@ export const useFetch = (url) => {
     return () => {
       controller.abort();
     };
-  }, [url]);
+  }, [url, options]);
 
   return { data, isPending, error };
 };
