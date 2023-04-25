3cf4e543a3e321ef1bb9f050835a0890bcd94254
Karolina Dbaj <k.dbaj@outlook.com>
2023-04-25 13:10:26 +0200

    Aborting Fetch Requests

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -6,11 +6,13 @@ export const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchData = async () => {
       setIsPending(true);
 
       try {
-        const res = await fetch(url);
+        const res = await fetch(url, { signal: controller.signal });
         if (!res.ok) {
           throw new Error(res.statusText);
         }
@@ -20,12 +22,19 @@ export const useFetch = (url) => {
         setData(json);
         setError(null);
       } catch (err) {
-        setIsPending(false);
-        setError("Could not fetch data");
-        console.log(err.message);
+        if (err.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch data");
+        }
       }
     };
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return { data, isPending, error };
